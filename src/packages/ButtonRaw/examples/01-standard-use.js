6e723a87c7d02c99e33da9e2f1146c39cc088f71
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2019-05-22 13:57:33 +0200

    docs corrected

--- src/packages/ButtonRaw/examples/01-standard-use.js
+++ src/packages/ButtonRaw/examples/01-standard-use.js
@@ -10,16 +10,25 @@ export default () => (
     <p>Raw button (no styling, just focus and accessibility)</p>
     <ButtonRaw>I'm a button</ButtonRaw>
 
-    <p>Raw button, custom styles</p>
+    <p>Raw button, clickable paragraph of text</p>
     <ButtonRaw
       css={css`
         font-size: 20px;
         border: 1px solid black;
         padding: 12px;
+        width: 300px;
       `}
       onClick={() => alert("hello")}
     >
-      I'm a styled button
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
     </ButtonRaw>
 
     <ThemeProvider
@@ -40,7 +49,17 @@ export default () => (
       }}
     >
       <p>Themed (outline on focus), pls focus this button</p>
-      <ButtonRaw>Some content here that is button with custom focus.</ButtonRaw>
+      <ButtonRaw onClick={() => alert("paragraph clicked")}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </ButtonRaw>
     </ThemeProvider>
   </div>
 );
