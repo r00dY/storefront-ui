f5a79046194053fd100e80c601612d82d6100a79
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2019-08-01 10:40:01 +0200

    using theme for notitications placement

--- src/packages/Notifications/index.js
+++ src/packages/Notifications/index.js
@@ -1,14 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
 import { rs } from "responsive-helpers";
 
-const NotificationsContext = React.createContext(null);
-
-import { useTheme } from "../Theme";
+import { ThemeContext } from "../Theme";
 
 let instance;
 
@@ -116,11 +113,13 @@ class NotificationsContainer extends React.Component {
     };
 
     return (
-      <div>
-        {typeof window !== "undefined" && window.innerWidth >= 768 && (
-          <>
-            <div
-              css={css`
+      <ThemeContext.Consumer>
+        {theme => (
+          <div>
+            {typeof window !== "undefined" && window.innerWidth >= 768 && (
+              <>
+                <div
+                  css={css`
                     position: fixed;
                     ${rs(theme.notifications.topLeft.y).css("top")}
                     ${rs(theme.notifications.topLeft.x).css("left")}
@@ -131,14 +130,14 @@ class NotificationsContainer extends React.Component {
                         )}
                     }
                 `}
-            >
-              {this.state.notifications
-                .filter(n => n.options.placement === "topLeft")
-                .map(n => n.element)}
-            </div>
+                >
+                  {this.state.notifications
+                    .filter(n => n.options.placement === "topLeft")
+                    .map(n => n.element)}
+                </div>
 
-            <div
-              css={css`
+                <div
+                  css={css`
                     position: fixed;
                     ${rs(theme.notifications.topRight.y).css("top")}
                     ${rs(theme.notifications.topRight.x).css("right")}
@@ -149,14 +148,14 @@ class NotificationsContainer extends React.Component {
                         )}
                     }
                 `}
-            >
-              {this.state.notifications
-                .filter(n => n.options.placement === "topRight")
-                .map(n => n.element)}
-            </div>
+                >
+                  {this.state.notifications
+                    .filter(n => n.options.placement === "topRight")
+                    .map(n => n.element)}
+                </div>
 
-            <div
-              css={css`
+                <div
+                  css={css`
                     position: fixed;
                     ${rs(theme.notifications.bottomRight.y).css("bottom")}
                     ${rs(theme.notifications.bottomRight.x).css("right")}
@@ -167,14 +166,14 @@ class NotificationsContainer extends React.Component {
                         )}
                     }
                 `}
-            >
-              {this.state.notifications
-                .filter(n => n.options.placement === "bottomRight")
-                .map(n => n.element)}
-            </div>
+                >
+                  {this.state.notifications
+                    .filter(n => n.options.placement === "bottomRight")
+                    .map(n => n.element)}
+                </div>
 
-            <div
-              css={css`
+                <div
+                  css={css`
                     position: fixed;
                     ${rs(theme.notifications.bottomLeft.y).css("bottom")}
                     ${rs(theme.notifications.bottomLeft.x).css("left")}
@@ -185,18 +184,18 @@ class NotificationsContainer extends React.Component {
                         )}
                     }
                 `}
-            >
-              {this.state.notifications
-                .filter(n => n.options.placement === "bottomLeft")
-                .map(n => n.element)}
-            </div>
-          </>
-        )}
+                >
+                  {this.state.notifications
+                    .filter(n => n.options.placement === "bottomLeft")
+                    .map(n => n.element)}
+                </div>
+              </>
+            )}
 
-        {typeof window !== "undefined" && window.innerWidth < 768 && (
-          <>
-            <div
-              css={css`
+            {typeof window !== "undefined" && window.innerWidth < 768 && (
+              <>
+                <div
+                  css={css`
                     position: fixed;
                     ${rs(theme.notifications.mobileBottom.y).css("bottom")}
                     ${rs(theme.notifications.mobileBottom.x).css("left")}
@@ -207,14 +206,14 @@ class NotificationsContainer extends React.Component {
                         )}
                     }
                 `}
-            >
-              {this.state.notifications
-                .filter(n => n.options.placementMobile === "bottom")
-                .map(n => n.element)}
-            </div>
+                >
+                  {this.state.notifications
+                    .filter(n => n.options.placementMobile === "bottom")
+                    .map(n => n.element)}
+                </div>
 
-            <div
-              css={css`
+                <div
+                  css={css`
                     position: fixed;
                     ${rs(theme.notifications.mobileTop.y).css("top")}
                     ${rs(theme.notifications.mobileTop.x).css("left")}
@@ -225,14 +224,16 @@ class NotificationsContainer extends React.Component {
                         )}
                     }
                 `}
-            >
-              {this.state.notifications
-                .filter(n => n.options.placementMobile === "top")
-                .map(n => n.element)}
-            </div>
-          </>
+                >
+                  {this.state.notifications
+                    .filter(n => n.options.placementMobile === "top")
+                    .map(n => n.element)}
+                </div>
+              </>
+            )}
+          </div>
         )}
-      </div>
+      </ThemeContext.Consumer>
     );
   }
 }
