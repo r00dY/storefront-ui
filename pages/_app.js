c0b00e8bcfce5d61857fc6796ac3d5b11cf13f61
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2019-10-22 12:33:17 +0200

    deployment

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,13 @@ import Root from "@commerce-ui/core/Root";
 import { theme } from "../demo/theme";
 
 export default class MyApp extends App {
+  static async getInitialProps(appContext) {
+    // calls page's `getInitialProps` and fills `appProps.pageProps`
+    const appProps = await App.getInitialProps(appContext);
+
+    return { ...appProps };
+  }
+
   render() {
     const { Component, pageProps } = this.props;
     return (
