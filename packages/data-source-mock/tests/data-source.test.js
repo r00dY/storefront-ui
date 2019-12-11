9cd88e994903fe2dd9860fec47c27d0f79b8a4f2
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2019-12-11 11:44:03 +0100

    data source generic test runner works in data-source-helpers

--- packages/data-source-mock/tests/data-source.test.js
+++ packages/data-source-mock/tests/data-source.test.js
@@ -1,10 +1,22 @@
-import "./init-test";
-import runTests from "./tests";
+import { ApolloClient, InMemoryCache, HttpLink } from "apollo-boost";
+import fetch from "cross-fetch";
+import "regenerator-runtime/runtime";
+import runTests from "@commerce-ui/data-source-helpers/runTests";
 
 import { getCollectionByHandle } from "../collectionByHandle";
 import { getCollections } from "../collections";
 import { getProducts } from "../products";
 import { getProductByHandle } from "../productByHandle";
+
+global.fetch = fetch;
+global.APOLLO_CLIENT = new ApolloClient({
+  connectToDevTools: process.browser,
+  link: new HttpLink({
+    uri: "http://localhost:4000" // Server URL (must be absolute)
+  }),
+  cache: new InMemoryCache(),
+  clientState: { defaults: {}, resolvers: {} }
+});
 
 const dataSource = {
   getCollectionByHandle,
