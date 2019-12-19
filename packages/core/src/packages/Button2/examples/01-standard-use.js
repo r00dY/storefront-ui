e77d941e408f8c2181d7ec9e03b0b59b1b9c2061
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2019-12-19 13:12:21 +0100

    button2 cfn

--- packages/core/src/packages/Button2/examples/01-standard-use.js
+++ packages/core/src/packages/Button2/examples/01-standard-use.js
@@ -1,6 +1,30 @@
+/** @jsx jsx */
+
 import React from "react";
-import { ButtonRaw } from "@commerce-ui/core/Button2";
+import {
+  ButtonRaw,
+  Button,
+  ThreeButtons,
+  ThreeButtons2,
+  ButtonSuper
+} from "@commerce-ui/core/Button2";
 import { Box } from "@commerce-ui/core/Box";
+import { jsx } from "@commerce-ui/core";
+
+const ButtonStyled = ({ color = "yellow", ...props }) => (
+  <Button
+    background={({ isHovered }) => ({
+      css: { backgroundColor: color, opacity: isHovered ? 0.5 : 1 }
+    })}
+    content={({ children }) => ({
+      css: {
+        p: 2
+      },
+      children: <span>{children} ty chuju</span>
+    })}
+    {...props}
+  />
+);
 
 export default () => {
   return (
@@ -49,6 +73,124 @@ export default () => {
       >
         Raw button with colors from a theme
       </ButtonRaw>
+
+      <p>Standard button</p>
+      <Button>Dupa dupa dupa</Button>
+
+      <p>Standard button</p>
+      <div
+        css={{
+          minWidth: "300px",
+          height: "200px"
+        }}
+      >
+        <Button fitWidth fitHeight>
+          Dupa dupa dupa
+        </Button>
+      </div>
+
+      <p>Styled button background</p>
+      <Button
+        background={{
+          css: { backgroundColor: "yellow" },
+          children: <div>Kurwa mać</div>
+        }}
+      >
+        Dupa dupa dupa
+      </Button>
+
+      <p>Styled button background</p>
+      <Button
+        background={({ isHovered }) => ({
+          css: { backgroundColor: "yellow", opacity: isHovered ? 0.5 : 1 }
+        })}
+        content={{
+          css: {
+            p: 2
+          }
+        }}
+      >
+        Dupa dupa dupa
+      </Button>
+
+      <p>Styled button custom children</p>
+      <Button
+        background={({ isHovered }) => ({
+          css: { backgroundColor: "yellow", opacity: isHovered ? 0.5 : 1 }
+        })}
+        content={({ children }) => ({
+          css: {
+            p: 2
+          },
+          children: <span>{children} ty chuju</span>
+        })}
+      >
+        Dupa dupa dupa
+      </Button>
+
+      <p>Three buttons</p>
+      <ThreeButtons />
+
+      <p>Three buttons overrides</p>
+      <ThreeButtons
+        button1={({ buttonProps }) => (
+          <Button
+            background={({ isHovered }) => ({
+              css: { backgroundColor: "yellow", opacity: isHovered ? 0.5 : 1 }
+            })}
+            content={({ children }) => ({
+              css: {
+                p: 2
+              },
+              children: <span>{children} ty chuju</span>
+            })}
+            {...buttonProps}
+          />
+        )}
+      />
+
+      <p>Three buttons with ButtonStyled</p>
+      <ThreeButtons
+        button1={({ buttonProps }) => <ButtonStyled {...buttonProps} />}
+      />
+
+      {/* above syntax is bad because it's not "overrideable", we can't add extra prop contextually without changing everything */}
+
+      <p>Three buttons 2, new API</p>
+      <ThreeButtons2 />
+
+      <p>Three buttons overrides</p>
+      <ThreeButtons2
+        button1={({ label }) => ({
+          props: {
+            background: ({ isHovered }) => ({
+              css: { backgroundColor: "yellow", opacity: isHovered ? 0.5 : 1 }
+            }),
+            content: {
+              css: {
+                p: 2
+              },
+              children: <span>--- {label} ---</span>
+            }
+          }
+        })}
+      />
+
+      <p>Three buttons overrides</p>
+      <ThreeButtons2
+        button1={({ label }) => ({
+          type: ButtonStyled,
+          props: {
+            color: "red"
+          },
+          children: <span>--- {label} ---</span>
+        })}
+      />
+
+      <p>ButtonSuper</p>
+      <ButtonSuper startEnhancer={"[A]"} endEnhancer={"[B]"}>
+        Dupa
+      </ButtonSuper>
     </div>
   );
 };
--- packages/core/src/packages/Button2/index.js
+++ packages/core/src/packages/Button2/index.js
@@ -1,7 +1,8 @@
 /** @jsx jsx */
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import { jsx } from "../index";
+import useHover from "../useHover";
 
 const buttonResetStyles = {
   border: "none",
@@ -21,11 +22,294 @@ const buttonResetStyles = {
 };
 
 function ButtonRaw_(props) {
-  const { css, ...restProps } = props;
+  const { css, innerRef, ...restProps } = props;
 
-  return <button css={[buttonResetStyles, css]} {...restProps} />;
+  return (
+    <button css={[buttonResetStyles, css]} {...restProps} ref={innerRef} />
+  );
 }
 
-const ButtonRaw = React.forwardRef(ButtonRaw_);
+const ButtonRaw = React.forwardRef((props, ref) => (
+  <ButtonRaw_ {...props} innerRef={ref} />
+));
 
 export { ButtonRaw };
+
+/**
+ * Button!
+ */
+
+let defaults = {
+  background: {
+    type: "div",
+    css: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
+    }
+  },
+  content: ({ children }) => ({
+    type: "div",
+    css: {
+      position: "relative",
+      pointerEvents: "none"
+    },
+    children: children
+  })
+};
+
+function getElem(override, defaults, state, forcedProps = {}) {
+  override = typeof override === "function" ? override(state) : override;
+  defaults = typeof defaults === "function" ? defaults(state) : defaults;
+  forcedProps =
+    typeof forcedProps === "function" ? forcedProps(state) : forcedProps;
+
+  return jsx(override.type || defaults.type, {
+    ...defaults.props,
+    ...override.props,
+    ...forcedProps,
+    css: [defaults.css, override.css],
+    children: override.children || defaults.children,
+    type: undefined
+  });
+}
+
+function Button_(props) {
+  const {
+    disabled,
+    css,
+    innerRef,
+    background,
+    content,
+    children,
+    fitWidth,
+    fitHeight,
+    ...restProps
+  } = props;
+
+  const [hoverRef, isHovered] = useHover();
+
+  const state = {
+    disabled,
+    isHovered,
+    active: false,
+    children
+  };
+
+  // TODO: isHovered is going crazy, unless we set pointerEvents: none to background and button.
+  // console.log(isHovered);
+
+  // top: -3,
+  // left: -3,
+  // right: -3,
+  // bottom: -3,
+  // background: "lightgrey",
+  // opacity: isHovered ? 0.2 : 0,
+  // transition: "opacity .2s"
+
+  // TODO: css props should be limited to layout ones.
+  return (
+    <ButtonRaw
+      {...restProps}
+      css={{
+        position: "relative",
+        width: fitWidth ? "100%" : "auto",
+        height: fitHeight ? "100%" : "auto"
+      }}
+      ref={hoverRef}
+    >
+      {getElem(background, defaults.background, state)}
+      {getElem(content, defaults.content, state)}
+
+      {/*<div css={{*/}
+      {/*position: "absolute",*/}
+      {/*top: 0,*/}
+      {/*left: 0,*/}
+      {/*right: 0,*/}
+      {/*bottom: 0*/}
+      {/*// top: -3,*/}
+      {/*// left: -3,*/}
+      {/*// right: -3,*/}
+      {/*// bottom: -3,*/}
+      {/*// background: "lightgrey",*/}
+      {/*// opacity: isHovered ? 0.2 : 0,*/}
+      {/*// transition: "opacity .2s"*/}
+      {/*}}>*/}
+
+      {/*</div>*/}
+
+      {/*<div css={{*/}
+      {/*position: "relative",*/}
+      {/*pointerEvents: "none"*/}
+      {/*// padding: 3*/}
+      {/*}}>*/}
+      {/*{children}*/}
+      {/*</div>*/}
+    </ButtonRaw>
+  );
+}
+
+Button_.defaultProps = {
+  disabled: false,
+  background: {},
+  content: {}
+};
+
+const Button = React.forwardRef((props, ref) => (
+  <Button_ innerRef={ref} {...props} />
+));
+
+export { Button };
+
+/**
+ * Button with loader and enhancers
+ */
+
+const ButtonSuper = ({ startEnhancer, endEnhancer, isLoading, ...props }) => (
+  <Button {...props} />
+);
+
+export { ButtonSuper };
+
+/**
+    Three buttons which onClick change label on the button on the right!!!
+ */
+
+function ThreeButtons({ button1, button2, button3 }) {
+  const [label1, setLabel1] = useState("dupa");
+  const [label2, setLabel2] = useState("dupa2");
+  const [label3, setLabel3] = useState("dupa3");
+
+  const onClick = () => {
+    setLabel1(label2);
+    setLabel2(label3);
+    setLabel3(label1);
+  };
+
+  const button1Props = {
+    onClick,
+    children: label1
+  };
+
+  const button2Props = {
+    onClick,
+    children: label2
+  };
+
+  const button3Props = {
+    onClick,
+    children: label3
+  };
+
+  return (
+    <div
+      css={{
+        display: "flex",
+        flexDirection: "row"
+      }}
+    >
+      <div css={{ flex: "1 1" }}>{button1({ buttonProps: button1Props })}</div>
+      <div css={{ flex: "1 1" }}>{button2({ buttonProps: button2Props })}</div>
+      <div css={{ flex: "1 1" }}>{button3({ buttonProps: button3Props })}</div>
+    </div>
+  );
+}
+
+ThreeButtons.defaultProps = {
+  button1: ({ buttonProps }) => <Button {...buttonProps} />,
+  button2: ({ buttonProps }) => <Button {...buttonProps} />,
+  button3: ({ buttonProps }) => <Button {...buttonProps} />
+};
+
+export { ThreeButtons };
+
+/**
+ ThreeButtons2 - different API
+ **/
+
+/**
+ Three buttons which onClick change label on the button on the right!!!
+ */
+
+defaults = {
+  ...defaults,
+  button: ({ label }) => ({
+    type: Button,
+    children: label
+  })
+};
+
+function ThreeButtons2({ button1, button2, button3 }) {
+  const [label1, setLabel1] = useState("dupa");
+  const [label2, setLabel2] = useState("dupa2");
+  const [label3, setLabel3] = useState("dupa3");
+
+  const onClick = () => {
+    setLabel1(label2);
+    setLabel2(label3);
+    setLabel3(label1);
+  };
+
+  // Forced props
+  const button1Props = {
+    onClick,
+    children: label1,
+    fitWidth: true,
+    fitHeight: true
+  };
+
+  const button1State = {
+    label: label1
+  };
+
+  const button2Props = {
+    onClick,
+    children: label2,
+    fitWidth: true,
+    fitHeight: true
+  };
+
+  const button2State = {
+    label: label2
+  };
+
+  const button3Props = {
+    onClick,
+    children: label3,
+    fitWidth: true,
+    fitHeight: true
+  };
+
+  const button3State = {
+    label: label3
+  };
+
+  return (
+    <div
+      css={{
+        display: "flex",
+        flexDirection: "row"
+      }}
+    >
+      <div css={{ flex: "1 1" }}>
+        {getElem(button1, defaults.button, button1State, button1Props)}
+      </div>
+      <div css={{ flex: "1 1" }}>
+        {getElem(button2, defaults.button, button2State, button2Props)}
+      </div>
+      <div css={{ flex: "1 1" }}>
+        {getElem(button3, defaults.button, button3State, button3Props)}
+      </div>
+    </div>
+  );
+}
+
+ThreeButtons2.defaultProps = {
+  button1: {},
+  button2: {},
+  button3: {}
+};
+
+export { ThreeButtons2 };
