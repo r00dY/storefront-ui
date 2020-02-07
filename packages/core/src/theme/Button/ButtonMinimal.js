adf5fade63e85c59ced5256a032833d848cd881d
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2020-02-07 13:17:38 +0100

    button minimal works

--- packages/core/src/theme/Button/ButtonMinimal.js
+++ packages/core/src/theme/Button/ButtonMinimal.js
@@ -1,24 +1,16 @@
-import { Button$ } from "@commerce-ui/core/Button";
+import ButtonText$ from "@commerce-ui/core/ButtonText";
+import { createComponent } from "@commerce-ui/core";
 import Spinner from "../Spinner";
 
-const Button = props => (
-  <Button$
-    {...props}
-    sx={{
-      ...props.sx,
+export default createComponent(ButtonText$, {
+  $body: ({ isHovered, disabled }) => ({
+    color: disabled ? "mono500" : "black",
+    font: "body2",
+    textAlign: "center",
+    opacity: isHovered ? 0.75 : 1
+  }),
 
-      $foreground: ({ isHovered, disabled }) => ({
-        color: disabled ? "mono500" : "black",
-        font: "body2",
-        textAlign: "center",
-        opacity: isHovered ? 0.75 : 1
-      }),
-
-      $loader: {
-        __children: <Spinner />
-      }
-    }}
-  />
-);
-
-export default Button;
+  $loader: {
+    __children: <Spinner />
+  }
+});
