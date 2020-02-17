435706b644dd16bb3e44706a218c9cce0ce4c11a
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2020-02-17 13:38:41 +0100

    publish updated

--- publish.sh
+++ publish.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# bump version of packages
 lerna version patch
 
+# build and publish core
 cd packages/core2
 npm run build
 npm publish --access public dist/
 
+# publish docs to Now
 cd ../docs
 now --target production --scope commerce-ui
