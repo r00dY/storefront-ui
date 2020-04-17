31c8335b761b8d4319eaa5198b934a3283e577b0
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2020-04-17 11:30:43 +0200

    open close

--- packages/core2/src/packages/MenuLayout/index.js
+++ packages/core2/src/packages/MenuLayout/index.js
@@ -5,6 +5,74 @@ function MenuBar(props) {
   return <Box>{props.children}</Box>;
 }
 
+const isBarOpen = bar => {
+  let open = bar.props.open;
+  if (typeof open === "undefined") {
+    open = true;
+  } else {
+    open = !!open;
+  }
+
+  return open;
+};
+
+const MenuBarsContainer = ({ bars, isPreviousOpen = true }) => {
+  if (!bars || bars.length === 0) {
+    return null;
+  }
+
+  const bar = bars[0];
+  const open = isBarOpen(bar);
+
+  return (
+    <Box sx={{ position: "relative" }}>
+      <Box
+        sx={{
+          transform: !open ? "translateY(-100%)" : "none",
+          transition: "transform .35s cubic-bezier(0.19, 1, 0.22, 1)"
+        }}
+      >
+        {bar}
+        <Box
+          sx={{
+            position: "absolute",
+            top: "100%",
+            width: "100%",
+            zIndex: -1
+          }}
+        >
+          <MenuBarsContainer bars={bars.slice(1)} />
+        </Box>
+      </Box>
+    </Box>
+  );
+
+  /**
+   * Below prototype is responsible for being able to hide 2 bars at the same time without harmonica effect.
+   *
+   * This has couple of problems:
+   * 1. Sometimes it's not animated (because of jumping from absolute to relative animation doest fire
+   * 2. If C is animating under B and then suddenly B becomes hidden too how do we do it without jank?
+   *
+   *
+   */
+  // const open = isBarOpen(bar);
+  // const nextBarOpen = bars.length > 1 && isBarOpen(bars[1]);
+  //
+  // return <Box sx={{position: "relative"}}>
+  //     <Box sx={{transform: !open && isPreviousOpen ? "translateY(-100%)" : "none", transition: "transform .35s cubic-bezier(0.19, 1, 0.22, 1)"}}>
+  //         { bar }
+  //         <Box sx={{
+  //             position: !open && !nextBarOpen ? "relative" : "absolute",
+  //             top: !open && !nextBarOpen ? 0 : "100%",
+  //             width: "100%",
+  //             zIndex: -1}}>
+  //             <MenuBarsContainer bars={bars.slice(1)} isPreviousOpen={open} />
+  //         </Box>
+  //     </Box>
+  // </Box>
+};
+
 function MenuLayout(props) {
   let { offset = 0 } = props;
 
@@ -19,12 +87,28 @@ function MenuLayout(props) {
     }
   });
 
+  console.log("--");
   return (
     <Box sx={{ position: "relative" }}>
       <Box
         sx={{ position: "fixed", top: 0, left: 0, width: "100%", zIndex: 100 }}
       >
-        {fixedBars}
+        <MenuBarsContainer bars={fixedBars} />
+        {/*{fixedBars.map(bar => {*/}
+
+        {/*const open = bar.props.open;*/}
+
+        {/*if (typeof open === 'undefined') {*/}
+        {/*return bar;*/}
+        {/*}*/}
+
+        {/*if (!open) {*/}
+        {/*return null;*/}
+        {/*}*/}
+
+        {/*return bar;*/}
+
+        {/*})}*/}
       </Box>
       <Box sx={{ pt: offset, position: "relative", zIndex: 0 }}>{children}</Box>
     </Box>
--- packages/core2/src/theme/MenuLayout.stories.js
+++ packages/core2/src/theme/MenuLayout.stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { jsx, rs } from "@commerce-ui/core";
 
 import Box from "@commerce-ui/core/Box";
@@ -7,95 +7,67 @@ import MenuLayout from "@commerce-ui/core/MenuLayout";
 
 const LOREM =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-const VeryLongContent = () => (
+const VeryLongContent = props => (
   <Box sx={{ m: [16, null, 50], maxWidth: 500 }}>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
-    <Box as={"p"} sx={{ mb: 20 }}>
-      {LOREM}
-    </Box>
+    {props.children}
+    {[...Array(20)].map((e, i) => (
+      <Box as={"p"} sx={{ mt: 20 }} key={i}>
+        {LOREM}
+      </Box>
+    ))}
   </Box>
 );
 
-export const basic = () => (
-  <MenuLayout offset={50}>
-    <VeryLongContent />
-    <MenuLayout.MenuBar>
-      <Box sx={{ bg: "lightblue", height: 50 }} />
-    </MenuLayout.MenuBar>
-    <VeryLongContent />
-    <MenuLayout.MenuBar>
-      <Box sx={{ bg: "coral", height: 50 }} />
-    </MenuLayout.MenuBar>
-  </MenuLayout>
-);
+export const basic = () => {
+  const [menu1Open, setMenu1Open] = useState(true);
+  const [menu2Open, setMenu2Open] = useState(true);
+  const [menu3Open, setMenu3Open] = useState(true);
+
+  return (
+    <MenuLayout offset={150}>
+      <MenuLayout.MenuBar open={menu1Open}>
+        <Box sx={{ bg: "lightblue", height: 50 }} />
+      </MenuLayout.MenuBar>
+
+      <MenuLayout.MenuBar open={menu2Open}>
+        <Box sx={{ bg: "coral", height: 50 }} />
+      </MenuLayout.MenuBar>
+
+      <MenuLayout.MenuBar open={menu3Open}>
+        <Box sx={{ bg: "antiquewhite", height: 50 }} />
+      </MenuLayout.MenuBar>
+
+      <VeryLongContent>
+        {/*<Box sx={{position: "relative", border: "1px dotted black"}}>*/}
+        {/*<Box sx={{transform: "translateY(-100%)"}}>*/}
+        {/*<Box sx={{bg: "antiquewhite", height: 50}}>*/}
+        {/*</Box>*/}
+        {/*<Box sx={{position: "absolute", width: "100%", top: "100%", zIndex: -1}}>*/}
+        {/*<Box sx={{transform: "translateY(-100%)"}}>*/}
+        {/*<Box sx={{bg: "coral", height: 50}}>*/}
+        {/*</Box>*/}
+        {/*</Box>*/}
+        {/*</Box>*/}
+        {/*</Box>*/}
+        {/*</Box>*/}
+        <button onClick={() => setMenu1Open(!menu1Open)}>Menu 1 toggle</button>
+        &nbsp;
+        <button onClick={() => setMenu2Open(!menu2Open)}>Menu 2 toggle</button>
+        &nbsp;
+        <button onClick={() => setMenu3Open(!menu3Open)}>Menu 3 toggle</button>
+        &nbsp;
+        <button
+          onClick={() => {
+            setMenu2Open(!menu2Open);
+            setMenu3Open(!menu3Open);
+          }}
+        >
+          Toggle 2 & 3
+        </button>
+      </VeryLongContent>
+    </MenuLayout>
+  );
+};
 
 export default {
   title: "MenuLayout"
