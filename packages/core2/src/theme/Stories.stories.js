41431e67788844359fe9788649d76b799b454993
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2020-07-10 11:57:54 +0200

    stories

---
+++ packages/core2/src/theme/Stories.stories.js
@@ -0,0 +1,16 @@
+import React, { useState, useRef, useEffect, useLayoutEffect } from "react";
+
+import Box from "@commerce-ui/core/Box";
+import { useTheme } from "@commerce-ui/core/Theme";
+
+export const story = () => {
+  return <div>Story</div>;
+};
+
+export const readme = () => {
+  return <div>Read me</div>;
+};
+
+export default {
+  title: "Stories.VariantPicker.story1"
+};
