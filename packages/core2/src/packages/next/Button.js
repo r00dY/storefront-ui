7612c365303fd1a4cfc5397f3230c1c32c77007d
Andrzej Dąbrowski <a.r.dabrowski@gmail.com>
2020-07-21 09:26:22 +0200

    next links

--- packages/core2/src/packages/next/Button.js
+++ packages/core2/src/packages/next/Button.js
@@ -1,6 +1,60 @@
 import React from "react";
+import Button from "../Button";
+import NextLink from "next/link";
 
-import ButtonProvider from "../Button/Button";
-import Link from "./Link";
+const NextButton = React.forwardRef((props, ref) => {
+  const {
+    href,
+    external = false,
+    forceLink = false,
+    as,
+    passHref,
+    prefetch,
+    replace,
+    scroll,
+    shallow,
+    ...restProps
+  } = props;
 
-export default ButtonProvider(Link);
+  const nextLinkProps = { href, as, prefetch, replace, scroll, shallow };
+
+  if ((href || forceLink) && !external) {
+    return (
+      <NextLink {...nextLinkProps} passHref>
+        <Button {...restProps} href={href} ref={ref} />
+      </NextLink>
+    );
+  }
+
+  return <Button {...restProps} ref={ref} href={href} />;
+});
+
+export default NextButton;
+
+// function NewLink(props) {
+//     const {
+//         href,
+//         as,
+//         passHref,
+//         prefetch,
+//         replace,
+//         scroll,
+//         shallow,
+//         _ref,
+//         ...restProps
+//     } = props;
+//
+//     const nextLinkProps = { href, as, prefetch, replace, scroll, shallow };
+//
+//     return (
+//         <LinkNext {...nextLinkProps} passHref>
+//             <Link {...restProps} href={href} ref={_ref} />
+//         </LinkNext>
+//     );
+// }
+//
+// export default React.forwardRef((props, ref) => (
+//     <NewLink {...props} _ref={ref} />
+// ));
+//
+//
--- packages/core2/src/packages/next/Link.js
+++ packages/core2/src/packages/next/Link.js
@@ -1,29 +1,32 @@
 import React from "react";
-import Link from "../Link";
-import LinkNext from "next/link";
+import Button from "./Button";
 
-function NewLink(props) {
-  const {
-    href,
-    as,
-    passHref,
-    prefetch,
-    replace,
-    scroll,
-    shallow,
-    _ref,
-    ...restProps
-  } = props;
-
-  const nextLinkProps = { href, as, prefetch, replace, scroll, shallow };
-
-  return (
-    <LinkNext {...nextLinkProps} passHref>
-      <Link {...restProps} href={href} ref={_ref} />
-    </LinkNext>
-  );
-}
+// function NewLink(props) {
+//   const {
+//     href,
+//     as,
+//     passHref,
+//     prefetch,
+//     replace,
+//     scroll,
+//     shallow,
+//     _ref,
+//     ...restProps
+//   } = props;
+//
+//   const nextLinkProps = { href, as, prefetch, replace, scroll, shallow };
+//
+//   return (
+//     <LinkNext {...nextLinkProps} passHref>
+//       <Link {...restProps} href={href} ref={_ref} />
+//     </LinkNext>
+//   );
+// }
+//
+// export default React.forwardRef((props, ref) => (
+//   <NewLink {...props} _ref={ref} />
+// ));
 
 export default React.forwardRef((props, ref) => (
-  <NewLink {...props} _ref={ref} />
+  <Button {...props} forceLink={true} __minimalLinkStyling={true} ref={ref} />
 ));
